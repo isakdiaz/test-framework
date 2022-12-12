861e973e9aa7f56aad5addb1a1cf7fcf73b09ea7
Isak Diaz <isakdiaz@gmail.com>
2022-12-12 18:09:53 -0500

    Initial Commit

---
+++ test-framework/test_framework.h
@@ -0,0 +1,18 @@
+//
+//  test_framework.h
+//  test-framework
+//
+//  Created by Isak Diaz on 12/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test_framework.
+FOUNDATION_EXPORT double test_frameworkVersionNumber;
+
+//! Project version string for test_framework.
+FOUNDATION_EXPORT const unsigned char test_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_framework/PublicHeader.h>
+
+
